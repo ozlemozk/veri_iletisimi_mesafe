c3480e3e0041b9b299aadf74c0c0f27a30cfd82a
Özlem ÖZKAYA <58657181+ozlemozk@users.noreply.github.com>
2020-12-26 22:14:21 +0300

    Add files via upload

---
+++ Veri_İletisimi_Mesafe/Veri_İletisimi_Mesafe/Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace Veri_İletisimi_Mesafe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btn_gös = new System.Windows.Forms.Button();
+            this.btn_kayıt = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.btn_sil = new System.Windows.Forms.Button();
+            this.btn_basla = new System.Windows.Forms.Button();
+            this.btn_dur = new System.Windows.Forms.Button();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(1, 280);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(134, 24);
+            this.label2.TabIndex = 40;
+            this.label2.Text = "Kayıtlı Veriler";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // btn_gös
+            // 
+            this.btn_gös.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btn_gös.Location = new System.Drawing.Point(535, 339);
+            this.btn_gös.Name = "btn_gös";
+            this.btn_gös.Size = new System.Drawing.Size(176, 56);
+            this.btn_gös.TabIndex = 38;
+            this.btn_gös.Text = "Veri Tabanındaki Verileri Göster";
+            this.btn_gös.UseVisualStyleBackColor = true;
+            this.btn_gös.Click += new System.EventHandler(this.btn_gös_Click);
+            // 
+            // btn_kayıt
+            // 
+            this.btn_kayıt.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btn_kayıt.Location = new System.Drawing.Point(535, 213);
+            this.btn_kayıt.Name = "btn_kayıt";
+            this.btn_kayıt.Size = new System.Drawing.Size(176, 56);
+            this.btn_kayıt.TabIndex = 37;
+            this.btn_kayıt.Text = "Veri Tabanına Kaydet";
+            this.btn_kayıt.UseVisualStyleBackColor = true;
+            this.btn_kayıt.Click += new System.EventHandler(this.btn_kayıt_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(12, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 24);
+            this.label1.TabIndex = 32;
+            this.label1.Text = "Değer";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 700;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // btn_sil
+            // 
+            this.btn_sil.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btn_sil.Location = new System.Drawing.Point(535, 277);
+            this.btn_sil.Name = "btn_sil";
+            this.btn_sil.Size = new System.Drawing.Size(176, 56);
+            this.btn_sil.TabIndex = 30;
+            this.btn_sil.Text = "TÜM VERİLERİ SİL";
+            this.btn_sil.UseVisualStyleBackColor = true;
+            this.btn_sil.Click += new System.EventHandler(this.btn_sil_Click);
+            // 
+            // btn_basla
+            // 
+            this.btn_basla.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btn_basla.Location = new System.Drawing.Point(535, 89);
+            this.btn_basla.Name = "btn_basla";
+            this.btn_basla.Size = new System.Drawing.Size(176, 56);
+            this.btn_basla.TabIndex = 29;
+            this.btn_basla.Text = "Başla";
+            this.btn_basla.UseVisualStyleBackColor = true;
+            this.btn_basla.Click += new System.EventHandler(this.btn_basla_Click);
+            // 
+            // btn_dur
+            // 
+            this.btn_dur.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btn_dur.Location = new System.Drawing.Point(535, 151);
+            this.btn_dur.Name = "btn_dur";
+            this.btn_dur.Size = new System.Drawing.Size(176, 56);
+            this.btn_dur.TabIndex = 28;
+            this.btn_dur.Text = "Dur";
+            this.btn_dur.UseVisualStyleBackColor = true;
+            this.btn_dur.Click += new System.EventHandler(this.btn_dur_Click);
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "TARİH";
+            this.Column3.Name = "Column3";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "SAAT";
+            this.Column2.Name = "Column2";
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Ölcüm (cm)";
+            this.Column4.Name = "Column4";
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "sıra";
+            this.Column1.Name = "Column1";
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(5, 328);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(427, 154);
+            this.dataGridView2.TabIndex = 39;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column4,
+            this.Column2,
+            this.Column3});
+            this.dataGridView1.Location = new System.Drawing.Point(5, 64);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(445, 194);
+            this.dataGridView1.TabIndex = 31;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(830, 597);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btn_gös);
+            this.Controls.Add(this.btn_kayıt);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_sil);
+            this.Controls.Add(this.btn_basla);
+            this.Controls.Add(this.btn_dur);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btn_gös;
+        private System.Windows.Forms.Button btn_kayıt;
+        private System.Windows.Forms.Label label1;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button btn_sil;
+        private System.Windows.Forms.Button btn_basla;
+        private System.Windows.Forms.Button btn_dur;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ Veri_İletisimi_Mesafe/Veri_İletisimi_Mesafe/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Windows.Forms.DataVisualization.Charting;
+using System.Data.SqlClient;
+
+namespace Veri_İletisimi_Mesafe
+{
+    public partial class Form1 : Form
+    {
+        string gelen = "0";
+        DateTime yeni = DateTime.Now;
+        int zaman = 0;
+        int satir = 1;
+        int satirNo = 1;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dataGridView2.Visible = false;
+            label2.Visible = false;
+            serialPort1.Close();
+         DateTime yeni = DateTime.Now; // saat ve tarih bilgisini almak için tanımlıyoruz
+        }
+ private void btn_basla_Click(object sender, EventArgs e)
+        {
+            serialPort1.PortName = "COM8";
+            serialPort1.Open();
+             timer1.Enabled = true;
+        }
+
+        private void btn_dur_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+            serialPort1.Close();
+        }
+
+        private void btn_sil_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Rows.Clear();
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            gelen = serialPort1.ReadLine();
+          zaman = (zaman + 1);
+            satir = dataGridView1.Rows.Add();
+
+            dataGridView1.Rows[satir].Cells[0].Value = satirNo;
+            dataGridView1.Rows[satir].Cells[1].Value = gelen; //ölçüm
+
+            dataGridView1.Rows[satir].Cells[2].Value = yeni.ToLongTimeString(); //saat bilgisi
+            dataGridView1.Rows[satir].Cells[3].Value = yeni.ToShortDateString(); //tarih bilgisi
+            satir++;
+            satirNo++;
+                label1.Text = gelen;
+        }
+
+        private void btn_kayıt_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(@"Data Source=LAPTOP-1SLQI3BT\OZLEM;Initial Catalog=veri_iletsi;Integrated Security=True");
+            using (SqlCommand cmd = new SqlCommand("INSERT INTO mesafe VALUES(@sıra, @mesafe, @saat, @tarih)", con))
+            {
+
+                cmd.Parameters.Add(new SqlParameter("@sıra", SqlDbType.Int));
+                cmd.Parameters.Add(new SqlParameter("@mesafe", SqlDbType.Int));
+                cmd.Parameters.Add(new SqlParameter("@saat", SqlDbType.Time));
+                cmd.Parameters.Add(new SqlParameter("@tarih", SqlDbType.Date));
+                foreach (DataGridViewRow row in dataGridView1.Rows) //satır denetimi
+                {
+                    if (!row.IsNewRow)
+                    {
+                        cmd.Parameters["@sıra"].Value = row.Cells[0].Value;
+                        cmd.Parameters["@mesafe"].Value = row.Cells[1].Value;
+                        cmd.Parameters["@saat"].Value = row.Cells[2].Value;
+                        cmd.Parameters["@tarih"].Value = row.Cells[3].Value;
+                        con.Open();
+
+                        cmd.ExecuteNonQuery();
+                        con.Close();
+                    }
+                }
+                MessageBox.Show("Kayıt Başarılı");
+            }
+        }
+
+        private void btn_gös_Click(object sender, EventArgs e)
+        {
+            dataGridView2.Visible = true;
+            label2.Visible = true;
+
+
+            SqlConnection con = new SqlConnection(@"Data Source=LAPTOP-1SLQI3BT\OZLEM;Initial Catalog=veri_iletsi;Integrated Security=True");
+            SqlDataAdapter da = new SqlDataAdapter("select*from mesafe", con);
+            DataSet ds = new DataSet();
+
+
+            con.Open();
+            da.Fill(ds, "mesafe");
+            dataGridView2.DataSource = ds.Tables["mesafe"];
+            con.Close();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
